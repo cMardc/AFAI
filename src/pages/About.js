ce09d31ef10232cfe490f4dffe0ac5dbcb9fefdd
cMardc <merd.yedek@gmail.com>
2024-01-06 22:01:39 +0400

    Add # to route

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,13 +8,17 @@ function About() {
         title="AFAI" 
         main="Əsas" 
         about="Haqqında"
-        mainURL="/"
-        aboutURL="about"
+        mainURL="/#/"
+        aboutURL="/#/about"
         facebook="facebook_url"
         instagram="instagram_url"
-        whatsapp="whatsapp_url"  
+        whatsapp="https://wa.me/994774332160"  
       />
-      <h1>Hello, World!</h1>
+      <div className='center'>
+        <p>
+          UC Satışı üçün qurulmuş sayt. Bank hesabına pulu ödəyərək qəbzi whatsapp-a atın.
+        </p>
+      </div>
     </div>
   );
 }
